84dfb02820d467b4e86bd8b79eccb8ef608b0d8c
ThomasAnkcorn <thomasankcorn@gmail.com>
2017-10-21 20:46:34 +0100

    made pause/play buttons work

--- client/main.js
+++ client/main.js
@@ -1,24 +1,42 @@
-window.onload = () =>{
+window.onload = () => {
     const socket = io('http://192.168.1.72:4200')
-    getClick((event)=>{
-        console.log('click',{button:event})
-        socket.emit('click',{button:event})
+    getClick((event) => {
+        console.log('click', {button: event})
+        socket.emit('click', {button: event})
+        updateControls(event)
     });
 }
 
 const buttons = {
-    back:"fa fa-backward fa-5x",
-    pause:"fa fa-pause fa-5x",
-    play:"fa fa-play fa-5x",
-    shuffle:"fa fa-random fa-5x",
-    reverse:"fa fa-exchange fa-5x",
-    next:"fa fa-forward fa-5x"
-    
+    back: "fa fa-backward fa-5x",
+    pause: "fa fa-pause fa-5x",
+    play: "fa fa-play fa-5x",
+    shuffle: "fa fa-random fa-5x",
+    reverse: "fa fa-exchange fa-5x",
+    next: "fa fa-forward fa-5x"
+
 }
-function getClick(callback){
+function getClick(callback) {
     const controls = document.querySelector(".controls");
-    controls.addEventListener("click",(data)=>{
-        const event = Object.keys(buttons).find(el => data.target.className === buttons[el])
+    controls.addEventListener("click", (data) => {
+        const event = Object
+            .keys(buttons)
+            .find(el => data.target.className === buttons[el])
+            
         callback(event)
     });
+}
+
+function updateControls(event) {
+    if (event === 'pause') play();
+    if (event === 'play') pause();
+}
+
+function pause() {
+    const playButton = document.querySelector('.fa.fa-play.fa-5x')
+    playButton.className = buttons.pause
+}
+function play() {
+    const pauseButton = document.querySelector('.fa.fa-pause.fa-5x')
+    pauseButton.className = buttons.play
 }
